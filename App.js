5effb3d2fb0eb711990d8394856c5125e2ed304e
Ilya Apasov <114296942+Apasov49@users.noreply.github.com>
2023-11-10 14:23:08 +0200

    App.js

---
+++ App.js
@@ -0,0 +1,24 @@
+// App.js
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons';
+import HomeScreen from './HomeScreen';
+import SecondScreen from './SecondScreen';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Second" component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
